4beda7de0eb8f611294caced343ffdbd5595355f
EDDy <84034778+EDUARDOCHULIM2098@users.noreply.github.com>
2021-06-16 15:54:55 -0500

    Update Miniproyecto.php

--- codeingniter/app/Controllers/Miniproyecto.php
+++ codeingniter/app/Controllers/Miniproyecto.php
@@ -1,11 +1,130 @@
 <?php
+namespace App\Controllers; 
+use App\MOdels\GeneralModel; 
 
-namespace App\Controllers;
+clas General extentends BaseController{ 
+	
+public function index(){ 
+	
+$Model=new GeneralModel();
+$mensaje=session('mensaje'); 	
+$datos=$gModel->listarTodo(); 
+$data=["datos"->$datos, 
+"mensaje"=>$mensaje =
+       
+];
+return view('listado',$data);
 
-class Home extends BaseController
+]	
+	
+public function  obtenerDatos($id){
+$gModel=new GeneralModel(); 
+$data=["id"=>$id]; 
+$respuesta=>gModel->obtenerInformacion($data){ 
+	
+$datos=["datos"=>$respuesta];
+return view('actualiar',$datos);
+	
 {
-	public function index()
-	{
-		return view('welcome_message');
-	}
-}
+
+public function insertar(){ 
+$gModel=new GeneralModel(); 
+$data=[ 
+    "nombre"=>$_POST['nombre'],
+    "a_paterno"=>$POST['apaterno'], 
+ "a_materno"=>$POST['amaterno´], 	
+ ];	
+ $respuesta=$gModel->insert($data);
+	
+  if(respuesta >0){ 
+    return redirect()-> to(base_url('/index.php'))->witch('mensahe','0'); 
+}else{  
+     return redirect()-> to(base_url('/index.php'))->witch('mensaje','1');						  
+  }						      
+						    	
+}	
+	
+	
+	
+ public function actualizar(){ 
+ 
+        $gModel=new GeneralModel();
+        $data=[ 
+           "nombre"=>$_POST['nombre'], 
+	   "a_paterno"=>$_POST['apaterno'],
+	   "a_materno"={>$_POST['amaterno'], 
+	   
+	   ]; 
+	   $id=["id"=>$_POST[´id´]];
+	   $respuestas=$gModel->actualizar($data,$id); 
+	   
+	   
+	                 if($respuesta)} 
+	              return redirect()->to(base_url('/index.php'))->witch('mensaje','2');
+		}else{ 
+		    return redirect()->to(base_url('/index.php'))->witch('mensaje','3');
+		  }
+		  
+	      } 
+	      
+         public funtion eliminar($idPersona){ 
+	      $gModel=new GeneralModel(); 
+	      $id=["id"=>$idPersona]; 
+	      $respuesta= $gModel->eliminar($id);
+	     
+         if($respuestas){ 
+	   return return redirect()->to(base_url('/index.php'))->witch('mensaje','4');
+	 }else{ 	  
+	    return return redirect()->to(base_url('/index.php'))->witch('mensaje','5');
+	}  
+	        
+   }      
+}	      
+	     
+	      
+	      
+	     
+	
+	     
+	    
+	   
+	    
+	
+	
+	    
+	  
+	  
+	   
+	   
+	
+	
+	  
+	
+	  
+	  
+	   
+	  
+	   
+	  
+	   
+
+	
+	   
+	  
+	   
+	  
+	   
+	   
+	  
+	   
+	  
+	   
+	   
+	
+	  
+	   
+	   
+
+		       
+		       
+		     
